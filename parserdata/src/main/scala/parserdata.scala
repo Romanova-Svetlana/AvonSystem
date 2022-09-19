84da689d16b21140b7ae2bb9301fac76674a01f9
Svetlana Romanova <romanova.svetlana.s@yandex.ru>
2022-09-19 23:24:14 +0300

    ParserData 0.1.0: Parser run file. Файл запуска парсера.
    parserdata.scala
    Run a program to process data for a specific date and collect daily data.
    Запуск программы для обработки данных за определенную дату и ежедневного сбора данных.

---
+++ parserdata/src/main/scala/parserdata.scala
@@ -0,0 +1,49 @@
+package com.avonsystem.parserdata
+
+import scala.util.{Success, Failure}
+
+import com.avonsystem.utilities.{DateTime, LogUtil, Setting, FilesUtil}
+import FilesUtil.createDir
+import LogUtil.log
+import com.avonsystem.utilities.dbschema.{Country, Url}
+
+object ParserData extends App {
+
+  args match {
+    case Array(date, _*) => 
+      DateTime.dateParse(date) match { 
+        case Success(d) => // Date received successfully. Дата получена успешно.
+          thisDayParse(d)
+        case Failure(err) => 
+          log("ERROR", "Invalid date format. Enter the date in YYYY-MM-DD format. Неверный формат даты. Введите дату в формате ГГГГ-ММ-ДД.", true, List(err))
+      }     
+    case _ => // Daily data collection. Ежедневный сбор данных.
+      Country.nameDir match {
+        case Nill => log("ERROR", "Error getting list of countries. The program has been terminated. Ошибка получения списка стран. Работа программы прекращена.", true)
+        case c => log("info", "Data collection and saving to disk files. Сбор данных и сохранение в файлы на диске.", true)
+          dailyParse(c)
+      }
+    }
+
+  // Daily data collection. Ежедневный сбор данных.
+  def dailyParse(countries: List[String]) = {
+    val dateStart = DateTime.dateNow
+    val datePath = Setting.archivePath + dateStart + "/"
+
+    for (cd <- country) {
+      createDir(datePath + cd + "/main/") 
+      createDir(datePath + cd + "/promo/")
+      createDir(datePath + cd + "/promotions/")
+      createDir(datePath + cd + "/products/")
+    }
+
+    //saveUrltoFile.start(urlList)
+    //saveUrltoFile.start(UrlTmp.sUrl)
+  }
+
+  // обработка данных за определенную дату
+  def thisDayParse(date: java.time.LocalDate) = {
+    val datePath = Setting.archivePath + date + "/"
+  }
+
+}
