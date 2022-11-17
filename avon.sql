0b6af6a49467c4cbb5f3a5b24b54b6370041870d
Svetlana Romanova <romanova.svetlana.s@yandex.ru>
2022-11-18 01:13:40 +0300

    AvonSystem 0.1.2: Tables with a list of categories. Таблицы со списком категорий.
    - avon.sql
    The list of categories added into database.
    Список категорий добавлен в базу данных.

--- avon.sql
+++ avon.sql
@@ -2611,3 +2611,46 @@ CREATE TABLE urls_items (
 
 ALTER TABLE urls_items ADD CONSTRAINT urls_items_fk_countries_urls_id 
 FOREIGN KEY (countries_urls_id) REFERENCES countries_urls(countries_urls_id);
+
+CREATE TABLE brands_tmp (
+	brands_id serial,
+	countries_id smallint,
+	languages_id smallint,
+	name varchar(255),
+	PRIMARY KEY ("brands_id")
+);
+
+ALTER TABLE brands_tmp ADD CONSTRAINT brands_tmp_fk_countries_id 
+FOREIGN KEY (countries_id) REFERENCES countries(countries_id);
+
+ALTER TABLE brands_tmp ADD CONSTRAINT brands_tmp_fk_languages_id 
+FOREIGN KEY (languages_id) REFERENCES languages(languages_id);
+
+CREATE TABLE categories_tmp (
+	categories_id serial,
+	countries_id smallint,
+	languages_id smallint,
+	id int,
+	name varchar(255),
+	search_words varchar(255),
+	PRIMARY KEY (categories_id)
+);
+
+ALTER TABLE categories_tmp ADD CONSTRAINT categories_tmp_fk_countries_id 
+FOREIGN KEY (countries_id) REFERENCES countries(countries_id);
+
+ALTER TABLE categories_tmp ADD CONSTRAINT categories_tmp_fk_languages_id 
+FOREIGN KEY (languages_id) REFERENCES languages(languages_id);
+
+CREATE TABLE categories_parent_tmp (
+	categories_parent_id serial,
+	categories_id int,
+	parent_id int,
+	PRIMARY KEY (categories_parent_id)
+);
+
+ALTER TABLE categories_parent_tmp ADD CONSTRAINT categories_parent_tmp_fk_categories_id 
+FOREIGN KEY (categories_id) REFERENCES categories_tmp(categories_id);
+
+ALTER TABLE categories_parent_tmp ADD CONSTRAINT categories_parent_tmp_fk_parent_id 
+FOREIGN KEY (parent_id) REFERENCES categories_tmp(categories_id);
