5328969af8ece99222098212c5ca17ab4574239b
Svetlana Romanova <romanova.svetlana.s@yandex.ru>
2022-11-18 01:18:33 +0300

    Utilites 1.7.1: Processing strings to be added to the database. Обработка строк для добавления в базу данных.
    - textformat.scala
      Replacing single quotes in SQL queries.
      Замена одинарных кавычек в SQL запросах.

--- utilities/src/main/scala/textformat.scala
+++ utilities/src/main/scala/textformat.scala
@@ -5,4 +5,16 @@ trait TextFormat {
   def crupStr(s: String) = if (s != null) s.tail.init else ""
   def toCapitalLetter(s: String) = s.toLowerCase.split(" ").map(_.capitalize).mkString(" ")
   def tagP(s: String)= s.replaceAll("""<p>(.*)</p>""", "$1")
+
+  def replaceSql(s: String) = {
+    val rep = for { 
+      i <- s
+      c = i.toString match {
+        case "'" => "''"
+        case x => x
+      }
+    } yield c
+    rep.mkString 
+  }
+
 }
